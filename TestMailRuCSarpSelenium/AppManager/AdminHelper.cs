623718953e233f486cb4c7f59a21df8251cb6a71
Ануфриев Дмитрий <da@en-gy.ru>
2019-11-29 12:05:29 +0300

    Initial commit

---
+++ TestMailRuCSarpSelenium/AppManager/AdminHelper.cs
@@ -0,0 +1,12 @@
+﻿namespace UnitTest
+{
+    class AdminHelper : BaseHelper
+    {
+        private string baseUrl;
+
+        public AdminHelper(ApplicationManager manager, string baseUrl) : base(manager)
+        {
+            this.baseUrl = baseUrl;
+        }
+    }
+}
