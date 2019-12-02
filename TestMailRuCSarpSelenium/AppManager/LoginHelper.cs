634bb9a60b4bc6a13d81ec96055eb23e854a360a
Ануфриев Дмитрий <da@en-gy.ru>
2019-12-02 15:04:55 +0300

    Обновил тесты

--- TestMailRuCSarpSelenium/AppManager/LoginHelper.cs
+++ TestMailRuCSarpSelenium/AppManager/LoginHelper.cs
@@ -24,47 +24,72 @@ namespace UnitTest
 
         public bool Login(AccountData account)
         {
-            if (IsLogedIn())
+            try
             {
-                if (IsLogedIn(account))
+                if (IsLogedIn())
                 {
-                    driver.FindElement(By.Id("PH_logoutLink")).Click();
+                    if (IsLogedIn(account))
+                    {
+                        driver.FindElement(By.Id("PH_logoutLink")).Click();
+                    }
                 }
+
+                driver.FindElement(By.Id("mailbox:login")).Clear();
+                driver.FindElement(By.Id("mailbox:login")).SendKeys(account.Login);
+                driver.FindElement(By.Id("mailbox:submit")).Click();
+
+                driver.FindElement(By.Id("mailbox:password")).Clear();
+                driver.FindElement(By.Id("mailbox:password")).SendKeys(account.Password);
+                driver.FindElement(By.Id("mailbox:submit")).Click();
+                Thread.Sleep(5000);
+
+                // добавить проверку
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
             }
 
-            driver.FindElement(By.Id("mailbox:login")).Clear();
-            driver.FindElement(By.Id("mailbox:login")).SendKeys(account.Login);
-            driver.FindElement(By.Id("mailbox:submit")).Click();
-
-            driver.FindElement(By.Id("mailbox:password")).Clear();
-            driver.FindElement(By.Id("mailbox:password")).SendKeys(account.Password);
-            driver.FindElement(By.Id("mailbox:submit")).Click();
-            Thread.Sleep(5000);
-            return true;
+            return false;
         }
 
         public bool LoginFrame(AccountData account)
         {
-            if (IsLogedIn())
+            try
             {
-                if (IsLogedIn(account))
+                if (IsLogedIn())
                 {
-                    driver.FindElement(By.Id("PH_logoutLink")).Click();
+                    if (IsLogedIn(account))
+                    {
+                        driver.FindElement(By.Id("PH_logoutLink")).Click();
+                    }
                 }
+
+                driver.FindElement(By.Id("PH_authLink")).Click();
+                Thread.Sleep(2000);
+                var frames = driver.FindElements(By.TagName("iframe"));
+                driver.SwitchTo().Frame(frames.Count - 1);
+                driver.FindElement(By.Name("Login")).Clear();
+                driver.FindElement(By.Name("Login")).SendKeys(account.Login);
+                driver.FindElement(By.XPath("//html//body//div[1]//div[3]//div//div[3]//div//div[2]//div//form//div[2]//div[2]//div[3]//div//div[1]//button")).Click();
+
+                driver.FindElement(By.Name("Password")).Clear();
+                driver.FindElement(By.Name("Password")).SendKeys(account.Password);
+                driver.FindElement(By.XPath("//html//body//div[1]//div[3]//div//div[3]//div//div[2]//div//form//div[2]//div//div[3]//div//div[1]/div//button")).Click();
+                Thread.Sleep(5000);
+
+                // добавить проверку
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
             }
 
-            driver.FindElement(By.Id("PH_authLink")).Click();
-            Thread.Sleep(2000);
-            driver.SwitchTo().Frame(2);
-            driver.FindElement(By.Name("Login")).Clear();
-            driver.FindElement(By.Name("Login")).SendKeys(account.Login);
-            driver.FindElement(By.XPath("//html//body//div[1]//div[3]//div//div[3]//div//div[2]//div//form//div[2]//div[2]//div[3]//div//div[1]//button")).Click();
-
-            driver.FindElement(By.Name("Password")).Clear();
-            driver.FindElement(By.Name("Password")).SendKeys(account.Password);
-            driver.FindElement(By.XPath("//html//body//div[1]//div[3]//div//div[3]//div//div[2]//div//form//div[2]//div//div[3]//div//div[1]/div//button")).Click();
-            Thread.Sleep(5000);
-            return true;
+            return false;
         }
 
         public bool Logoff() // тут нужно еще подумать
--- TestMailRuCSarpSelenium/Tests/LoginTest.cs
+++ TestMailRuCSarpSelenium/Tests/LoginTest.cs
@@ -12,10 +12,8 @@ namespace UnitTest
         public bool LoginWithValidCredentials(string login, string password)
         {
             AccountData account = new AccountData(login, password);
-            //application.Authentication.Login(account);
-            application.Authentication.LoginFrame(account);
 
-            return true;
+            return application.Authentication.Login(account) && application.Authentication.LoginFrame(account);
         }
     }
 }
